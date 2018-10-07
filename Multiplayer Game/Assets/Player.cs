16d4f083a723318ea212b35ef67578caf2d519f7
iMiike1 <43544291+iMiike1@users.noreply.github.com>
2018-10-07 20:34:35 +0100

    test6854132

--- Multiplayer Game/Assets/Player.cs
+++ Multiplayer Game/Assets/Player.cs
@@ -5,34 +5,34 @@ using System.Collections;
 public class Player : Photon.MonoBehaviour {
 
     public float speed = 10f;
-    //private float lastSynchronizationTime = 0f;
-    //private float syncDelay = 0f;
-    //private float syncTime = 0f;
-    //private Vector3 syncStartPosition = Vector3.zero;
-    //private Vector3 syncEndPosition = Vector3.zero;
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+    private Vector3 syncStartPosition = Vector3.zero;
+    private Vector3 syncEndPosition = Vector3.zero;
 
-    //void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    //{
-    //    if (stream.isWriting)
-    //    {
-    //        stream.SendNext(GetComponent<Rigidbody>().position);
-    //        stream.SendNext(GetComponent<Rigidbody>().velocity);
-    //    }
-    //    else
-    //    {
-    //        Vector3 syncPosition = (Vector3)stream.ReceiveNext();
-    //        Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
-    //        syncTime = 0f;
-    //        syncDelay = Time.time - lastSynchronizationTime;
-    //        lastSynchronizationTime = Time.time;
-    //        syncEndPosition = syncPosition + syncVelocity * syncDelay;
-    //        syncStartPosition = GetComponent<Rigidbody>().position;
-    //    }
-    //}
-    //void Awake ()
-    //{
-    //    lastSynchronizationTime = Time.time;
-    //}
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(GetComponent<Rigidbody>().position);
+            stream.SendNext(GetComponent<Rigidbody>().velocity);
+        }
+        else
+        {
+            Vector3 syncPosition = (Vector3)stream.ReceiveNext();
+            Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
+            syncTime = 0f;
+            syncDelay = Time.time - lastSynchronizationTime;
+            lastSynchronizationTime = Time.time;
+            syncEndPosition = syncPosition + syncVelocity * syncDelay;
+            syncStartPosition = GetComponent<Rigidbody>().position;
+        }
+    }
+    void Awake()
+    {
+        lastSynchronizationTime = Time.time;
+    }
 
     void Start()
     {
@@ -45,10 +45,10 @@ public class Player : Photon.MonoBehaviour {
             InputMovement();
             InputColorChange();
         }
-        //else
-        //{
-        //    SyncedMovement();
-        //}
+        else
+        {
+            SyncedMovement();
+        }
     }
     void InputMovement()
     {
@@ -62,11 +62,11 @@ public class Player : Photon.MonoBehaviour {
             GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position - Vector3.right * speed * Time.deltaTime);  
     }
 
-    //private void SyncedMovement()
-    //{
-    //    syncTime += Time.deltaTime;
-    //    GetComponent<Rigidbody>().position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
-    //}
+    private void SyncedMovement()
+    {
+        syncTime += Time.deltaTime;
+        GetComponent<Rigidbody>().position = Vector3.Lerp(syncStartPosition, syncEndPosition, syncTime / syncDelay);
+    }
 
     private void InputColorChange()
     {
