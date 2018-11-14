8562ec77ca3e4e808ae9ffccacc33b760bf8b6b3
iMiike1 <43544291+iMiike1@users.noreply.github.com>
2018-11-14 20:06:31 +0000

    Revert "StillNotWorking"
    This reverts commit 845d58e10d53737af5e6bd8cbf4105b227b3c761.

--- DuckHunt/Assets/Scripts/NetworkManager.cs
+++ DuckHunt/Assets/Scripts/NetworkManager.cs
@@ -11,7 +11,6 @@ public class NetworkManager : MonoBehaviour
     public GameObject player;
     public GameObject defaultCamera;
     public GameObject pc;
-    public GameObject cam;
     
     //public GameObject ball;
 
@@ -83,12 +82,8 @@ public class NetworkManager : MonoBehaviour
     {
       GameObject mPLayer= (GameObject) PhotonNetwork.Instantiate(player.name, new Vector3(-4f, 1.5f, -2f), Quaternion.identity, 0);
         ((MonoBehaviour)mPLayer.GetComponent("PlayerController")).enabled = true;
+        
         mPLayer.transform.Find("MainCamera").gameObject.SetActive(true);
-        
-        
-        
-            //(mPLayer.GetComponent<"PlayerController"> (MonoBehaviour)).enabled = true;
-        
 
     }
 }
