00b748d4e7897aee9c02e1b9ea4d4d421b546424
iMiike1 <43544291+iMiike1@users.noreply.github.com>
2018-11-15 21:17:41 +0000

    HalfUIDone

---
+++ DuckHunt/Assets/Scripts/CreateRoom/CreateRoom1.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CreateRoom1 : MonoBehaviour {
+
+    [SerializeField]
+    private Text _roomName;
+    private Text RoomName
+    {
+        get { return _roomName; }
+    }
+
+    public void onClick_CreateRoom()
+    {
+        if (PhotonNetwork.CreateRoom(RoomName.text))
+        {
+            print("create room successfully sent.");
+        }
+        else
+        {
+            print("create room failed to send");
+        }
+
+    }
+
+    private void OnPhotonCreateRoomFailed(object[] codeAndMessage)
+    {
+        print("Create room failed" + codeAndMessage[1]);
+    }
+
+    void OnCreatedRoom()
+    {
+        print("Room created successfully.");
+    }
+}
---
+++ DuckHunt/Assets/Scripts/RoomLayoutGroup.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomLayoutGroup : MonoBehaviour {
+
+
+    [SerializeField]
+    private GameObject _roomListingPrefab;
+    private GameObject RoomListingPrefab
+    {
+        get { return _roomListingPrefab; }
+    }
+    private List<RoomListing> _roomListingButton = new List<RoomListing>();
+    private List<RoomListing> RoomListingButtons
+    {
+        get { return _roomListingButton; }
+    }
+
+
+    private void OnRecievedRoomListUpdate()
+    {
+        RoomInfo[] rooms = PhotonNetwork.GetRoomList();
+
+        foreach (RoomInfo room in rooms)
+        {
+            RoomRecieved(room);
+        }
+    }
+
+
+    private void RoomRecieved(RoomInfo room)
+    {
+    https://www.youtube.com/watch?v=SBVyCNpg9X8 (17.38)
+    }
+
+    private void RemoveOldRooms()
+    {
+
+    }
+
+}
