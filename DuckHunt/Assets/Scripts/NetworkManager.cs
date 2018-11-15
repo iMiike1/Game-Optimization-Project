9b9a5b7cff2d44a3dccfed857e38e83f7f8e669a
iMiike1 <43544291+iMiike1@users.noreply.github.com>
2018-11-15 13:28:09 +0000

    updates

--- DuckHunt/Assets/Scripts/NetworkManager.cs
+++ DuckHunt/Assets/Scripts/NetworkManager.cs
@@ -11,6 +11,7 @@ public class NetworkManager : MonoBehaviour
     public GameObject player;
     public GameObject defaultCamera;
     public GameObject pc;
+    public GameObject cam;
     
     //public GameObject ball;
 
@@ -81,10 +82,19 @@ public class NetworkManager : MonoBehaviour
     void SpawnPlayer()
     {
       GameObject mPLayer= (GameObject) PhotonNetwork.Instantiate(player.name, new Vector3(-4f, 1.5f, -2f), Quaternion.identity, 0);
+<<<<<<< HEAD
         //((MonoBehaviour)mPLayer.GetComponent("PlayerController")).enabled = true;
         mPLayer.GetComponent<UnityStandardAssets.Characters.FirstPerson.PlayerController>().enabled = true;
 
+=======
+        ((MonoBehaviour)mPLayer.GetComponent("PlayerController")).enabled = true;
+>>>>>>> parent of 8562ec7... Revert "StillNotWorking"
         mPLayer.transform.Find("MainCamera").gameObject.SetActive(true);
+        
+        
+        
+            //(mPLayer.GetComponent<"PlayerController"> (MonoBehaviour)).enabled = true;
+        
 
     }
 }
